1c243b173e4d7709fef918d2d8ae0ab097ac8dd9
Quentin Vansteene <vansteene.quentin@gmail.com>
2022-12-19 18:36:26 +0100

    style and script for header and cart button

--- apm.js
+++ apm.js
@@ -1,4 +1,34 @@
 $(function(){
-	var header = $('#header').headerFWOld('get');
+	var header;
+	if (app.components.includes('headerFWOld'))
+      	header = $('#header').headerFWOld('get');
+  	else if (app.components.includes('headerFW'))
+		header = $('#header').headerFW('get');
 	header.$stickyEl = $('#container');
+
+
+	$(window).resize(function(){
+		resetCartPosition()
+	});
+
+	
+	if ($('.addToCart__wrapper').length) {
+		var observer = new MutationObserver(function callback(mutationList, observer) {
+				mutationList.forEach(function(mutation) {
+					if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+						resetCartPosition()
+					}
+				})
+			}
+		).observe(header.$el.get(0), {
+		  		attributes: true
+			}
+		)
+	}
 });
+
+var resetCartPosition = function(){
+	$('.addToCart__wrapper').css({
+		'top' : $('.headerFW').outerHeight(),
+	})
+}
