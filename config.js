2ade733bfc30a76658e85a087a0a19c45aa87bfc
Quentin Vansteene <vansteene.quentin@gmail.com>
2024-03-06 17:13:33 +0100

    change some config options

--- config.js
+++ config.js
@@ -1,5 +1,24 @@
 module.exports = {
+	'spacing': '2rem',
 	'header': {
-		'font-size': '0.9em',
+		'font-size': '0.85em',
+	},
+	'btn': {
+		'radius': '30px',
+		'border-size':'1px',
+		'padding-y': '0.5em',
+		'padding-x': '1.5em',
+		'uppercase': 'false',
+	},
+	'input': {
+		'radius': '30px',
+		'border-size':'1px',
+	},
+	'footer': {
+		'font-size': '12px',
+	},
+	'link':{
+		'underline':'true',
+		'underline-on-hover':'true',
 	},
 };
